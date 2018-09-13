833e9ac6e28702c32d44cf87179148c695459a10
Manuel Emilio Carpio <mectwork@gmail.com>
2018-09-13 10:24:45 -0400

    Created basic joi validation on POST

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 var express = require('express');
 var app = express();
 const port = process.env.PORT || 5000;
@@ -41,6 +42,19 @@ app.get('/api/contacts/:id', function (req, res) {
 });
 
 app.post('/api/contacts', function (req, res) {
+    const schema = {
+        name: Joi.string().min(3).required(),
+        email: Joi.string().email().required(),
+        phone: Joi.string().required()
+    }
+
+    const validation = Joi.validate(req.body, schema);
+    console.log(validation);
+
+    if(validation.error){
+        res.status(400).send(validation.error.details[0].message);
+    }
+
     const contact = {
         id: contacts.length + 1,
         name: req.body.name,

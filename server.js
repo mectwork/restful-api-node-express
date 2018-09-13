1e63bd0dd37b42ddda75c06e95a9dec40f74f827
Manuel Emilio Carpio <mectwork@gmail.com>
2018-09-13 09:57:24 -0400

    Adding POST without input validation

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json());
+
 const contacts = [
     {
         id: 1,
@@ -36,6 +38,17 @@ app.get('/api/contacts/:id', function (req, res) {
         contact.id === parseInt(req.params.id));
     if (!contact) res.status(404).send("The requested contact doesn't exist.");
     res.status(200).send(contact);
+});
+
+app.post('/api/contacts', function (req, res) {
+    const contact = {
+        id: contacts.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        phone: req.body.phone
+    }
+    contacts.push(contact);
+    res.send(contact);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

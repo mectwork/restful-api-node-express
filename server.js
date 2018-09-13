b015be8fdef2c5b0db741d36b9f7764f2b488ee2
Manuel Emilio Carpio <mectwork@gmail.com>
2018-09-13 09:33:04 -0400

    Creating static array of contacts and implementing two GET requests

--- server.js
+++ server.js
@@ -2,8 +2,40 @@ var express = require('express');
 var app = express();
 const port = process.env.PORT || 5000;
 
+const contacts = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'john@gmail.com',
+        phone: '123-456-789'
+    },
+    {
+        id: 2,
+        name: 'Sonya Smith',
+        email: 'sonya@gmail.com',
+        phone: '789-789-789'
+    },
+    {
+        id: 3,
+        name: 'Juan Perez',
+        email: 'juan@gmail.com',
+        phone: '456-456-456'
+    }
+];
+
 app.get('/', function (req, res) {
     res.send('Hello Server World!!');
+});
+
+app.get('/api/contacts', function (req, res) {
+    res.send(contacts);
+});
+
+app.get('/api/contacts/:id', function (req, res) {
+    const contact = contacts.find(contact =>
+        contact.id === parseInt(req.params.id));
+    if (!contact) res.status(404).send("The requested contact doesn't exist.");
+    res.status(200).send(contact);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
